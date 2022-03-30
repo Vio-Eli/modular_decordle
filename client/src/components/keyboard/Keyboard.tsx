b86c712e686afcd2ee02a2f6c8c8c9e9a5205ce5
Eli Fischl <vio@ocf.berkeley.edu>
2022-03-29 21:18:23 -0700

    Installed gh-pages and made dict lowercase

--- client/src/components/keyboard/Keyboard.tsx
+++ client/src/components/keyboard/Keyboard.tsx
@@ -1,6 +1,34 @@
 import React from 'react';
 
+let keyboard: string[][] = [
+    ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+    ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'BACKSPACE']
+];
+
 export default function Keyboard(){
 
     
 }
+
+/*
+get color from checked[]
+key color = color
+*/
+
+/*export default function check(guess: string, ans: string): checked[] {
+    ans = ans.toLowerCase();
+    guess = guess.toLowerCase(); // Just to be safe. Should already be lowercase
+    let j: number;
+    return guess.split("").map((letter, i) => {
+        if (letter === ans[i]) {
+            // add class to letter
+            return { letter, color: 'green' }
+        } else if ((j = ans.indexOf(letter)) !== -1) {
+            ans = ans.slice(0, j) + "*" + ans.slice(j+1); // Remove letter at index j of correct answer
+            return { letter, color: 'goldenrod' }
+        } else {
+            return { letter, color: 'gray' }
+        }
+    });
+} */
